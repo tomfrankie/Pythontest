621b3be6c3a6a195503fc569dc1095ff157f87e3
tomfrankie <793365276@qq.com>
2014-10-17 11:26:09 +0800

    This is for test!

---
+++ test.py
@@ -0,0 +1,10 @@
+items = {
+    'number' : 42,
+    'text' : "Hello World"
+    }
+items["func"] = abs
+import math
+items["mod"] = math
+items["error"] = ValueError
+nums = [1,2,3,4]
+items["append"] = nums.append
